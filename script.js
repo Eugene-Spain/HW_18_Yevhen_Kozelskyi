946505fc751bb6b569bd4167a64020cc62cd040f
Eugene-Spain <zheka200909@gmail.com>
2024-02-13 00:36:18 +0100

    js task done

---
+++ script.js
@@ -0,0 +1,37 @@
+let nan;
+let ami = "ami";
+console.log(1 + nan + String(ami));
+
+let eugeneInfo = {
+    name:"Eugene",
+    nickname:"Spawn",
+    telephone:380935347176,
+    email:"zheka2009@gmail.com",
+    sex:"male",
+    hobby:"swimming",
+    married:"true"
+};
+let mariaInfo = {
+    name:"Maria",
+    nickname:"Santa",
+    telephone:380935019286,
+    email:"mariaK93@gmail.com",
+    sex:"female",
+    hobby:"riding",
+    married:"true"
+};
+let dmytroInfo = {
+    name:"Dmytro",
+    nickname:"StrainTod",
+    telephone:380935019938,
+    email:"dimdim1919@gmail.com",
+    sex:"male",
+    hobby:"PC games",
+    married:"false"
+};
+let family = [
+    eugeneInfo,
+    mariaInfo,
+    dmytroInfo
+];
+console.log(family);
